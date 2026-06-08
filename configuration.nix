45678dbf292013048a309831be3fcc1855355e31
anon <ilovearchbtw@proton.me>
2026-06-08 19:32:33 +0800

    clean

--- configuration.nix
+++ configuration.nix
@@ -1,68 +1,62 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "nixbox";
-
-  networking.networkmanager.enable = true;
-
-  # nvidia
-  hardware.graphics = {
-    enable = true;
+  # --- Bootloader ---
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
   };
-  
-  services.xserver.videoDrivers = ["nvidia"];
 
-  hardware.nvidia = {
-    modesetting.enable = true;
-  
-    powerManagement.enable = true;
-    powerManagement.finegrained = true;
-  
-    open = true;
-  
-    nvidiaSettings = true;
-    
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  
-    prime = {
-      offload = {
+  # --- Networking ---
+  networking = {
+    hostName = "nixbox";
+    networkmanager.enable = true;
+  };
+
+  # --- Hardware & Drivers ---
+  hardware = {
+    graphics.enable = true;
+    nvidia = {
+      modesetting.enable = true;
+      powerManagement = {
         enable = true;
-        enableOffloadCmd = true;
+        finegrained = true;
       };
-  
-      intelBusId = "PCI:0:2:0";
-      nvidiaBusId = "PCI:1:0:0";
+      open = true;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      prime = {
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
     };
   };
-  
-  # Set your time zone.
+
+  # --- Localization & Input Methods ---
   time.timeZone = "Asia/Taipei";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  i18n.inputMethod = {
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
+    inputMethod = {
       enable = true;
       type = "fcitx5";
       fcitx5 = {
@@ -73,68 +67,74 @@
         ];
       };
     };
+  };
 
-  virtualisation.docker.enable = true;
-  virtualisation.libvirtd.enable = true;
-  systemd.tmpfiles.rules = [ "L+ /var/lib/qemu/firmware - - - - ${pkgs.qemu}/share/qemu/firmware" ];
+  # --- Virtualization ---
+  virtualisation = {
+    docker.enable = true;
+    libvirtd.enable = true;
+  };
 
+  systemd.tmpfiles.rules = [
+    "L+ /var/lib/qemu/firmware - - - - ${pkgs.qemu}/share/qemu/firmware"
+  ];
+
+  # --- User Accounts ---
   users.users."anon" = {
     isNormalUser = true;
     description = "anon";
-    extraGroups = [ "networkmanager" "wheel" "docker"];
-    packages = with pkgs; [];
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    packages = with pkgs; [ ];
   };
 
+  # --- System Packages & Environment ---
   nixpkgs.config.allowUnfree = true;
 
-  environment.systemPackages = with pkgs; [
-     vim
-     wget
-     git
-     qemu
-     docker-compose
-     dnsmasq
-  ];
-
-  environment.sessionVariables = {
+  environment = {
+    systemPackages = with pkgs; [
+      vim
+      wget
+      git
+      qemu
+      docker-compose
+      dnsmasq
+    ];
+    sessionVariables = {
       XMODIFIERS = "@im=fcitx";
       GTK_IM_MODULE = "fcitx";
       QT_IM_MODULE = "fcitx";
     };
+  };
 
   fonts.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
   ];
 
+  # --- Programs ---
   programs = {
+    nix-ld.enable = true;
+    virt-manager.enable = true;
     slock = {
       enable = true;
-      package = (pkgs.slock.overrideAttrs (oldAttrs: {
+      package = pkgs.slock.overrideAttrs (oldAttrs: {
         src = ./config/slock;
-      }));
-    };
-    nix-ld = {
-      enable = true;
-    };
-    virt-manager = {
-      enable = true;
+      });
     };
   };
 
+  # --- System Services ---
   services = {
-
-    displayManager = {
-      ly.enable = true;
-    };
+    displayManager.ly.enable = true;
 
     xserver = {
       enable = true;
+      videoDrivers = [ "nvidia" ];
       autoRepeatDelay = 200;
       autoRepeatInterval = 35;
       xkb = {
-          layout = "jp";
-          variant = "";
-        };
+        layout = "jp";
+        variant = "";
+      };
 
       windowManager.dwm = {
         enable = true;
@@ -145,16 +145,13 @@
     };
   };
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ 22 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  nix.settings = {
-    experimental-features = [ "nix-command" "flakes" ];
-    auto-optimise-store = true;
+  # --- Nix Daemon Settings ---
+  nix = {
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      auto-optimise-store = true;
+    };
   };
-  system.stateVersion = "26.05";
 
+  system.stateVersion = "26.05";
 }
