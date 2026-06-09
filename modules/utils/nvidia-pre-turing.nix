4473083ddb4c7ebb41a3aa49541ddf9c8d0661e0
anon <ilovearchbtw@proton.me>
2026-06-09 20:29:25 +0800

    try fix nv

--- modules/utils/nvidia-pre-turing.nix
+++ modules/utils/nvidia-pre-turing.nix
@@ -7,11 +7,16 @@
       modesetting.enable = true;
       powerManagement = {
         enable = true;
+        finegrained = true;
       };
       open = false;
       nvidiaSettings = true;
       package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
       prime = {
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
         intelBusId = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
       };
