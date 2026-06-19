a76c796edffeb65a9dca0b8f6c9db8411dcff0c3
anon <ilovearchbtw@proton.me>
2026-06-19 22:07:19 +0800

    update

--- modules/pkgs/swayidle.nix
+++ modules/pkgs/swayidle.nix
@@ -6,26 +6,15 @@
   services.swayidle = {
     enable = true;
     
-    events = [
-      {
-        event = "before-sleep";
-        command = "swaylock -fF";
-      }
-      {
-        event = "lock";
-        command = "swaylock -fF";
-      }
-    ];
+    events = {
+      before-sleep = "swaylock -fF";
+      lock = "swaylock -fF";
+    };
 
     timeouts = [
       {
         timeout = 300;
         command = "swaylock -fF";
-      }
-      {
-        timeout = 600;
-        command = "wlr-randr --output --off"; 
-        resumeCommand = "wlr-randr --output --on";
       }
     ];
   };
