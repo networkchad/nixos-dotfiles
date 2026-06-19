49ae80018350232ed526fdefea9d35f7c9490cbe
anon <ilovearchbtw@proton.me>
2026-06-19 19:58:42 +0800

    sleep more

--- src/dwl/scripts/start-dwl.sh
+++ src/dwl/scripts/start-dwl.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
+
 fcitx5 -d &
 
-sleep 0.1
-
-swaybg -i "$HOME/.config/wallpapers/2077.png" -m fill &
+(sleep 1 && swaybg -i "$HOME/.config/wallpapers/2077.png" -m fill) &
 
 slstatus -s | exec dwl
