6793427edbfb74866a01dcecfb25a1a9ce98afb3
anon <ilovearchbtw@proton.me>
2026-06-19 17:27:18 +0800

    session

--- modules/pkgs/dwl.nix
+++ modules/pkgs/dwl.nix
@@ -11,4 +11,18 @@
       ];
     }))
   ];
+
+  services.displayManager.sessionPackages = [
+    (pkgs.writeTextFile {
+      name = "dwl-wayland-session";
+      destination = "/share/wayland-sessions/dwl.desktop";
+      text = ''
+        [Desktop Entry]
+        Name=dwl
+        Comment=Dynamic Window Manager for Wayland
+        Exec=dwl
+        Type=Application
+      '';
+    })
+  ];
 }
