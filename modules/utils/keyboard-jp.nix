98a06acce0327c7067a7a3a1c6e163afa07dc64d
anon <ilovearchbtw@proton.me>
2026-06-19 17:17:52 +0800

    update

--- modules/utils/keyboard-jp.nix
+++ modules/utils/keyboard-jp.nix
@@ -1,10 +1,12 @@
 { config, pkgs, ... }:
 
 {
+  environment.sessionVariables = {
+    XKB_DEFAULT_LAYOUT = "jp";
+  };
+
   services.xserver.xkb = {
     layout = "jp";
     variant = "";
   };
-
-  console.useXkbConfig = true;
 }
--- modules/utils/keyboard-us.nix
+++ modules/utils/keyboard-us.nix
@@ -1,10 +1,12 @@
 { config, pkgs, ... }:
 
 {
+  environment.sessionVariables = {
+    XKB_DEFAULT_LAYOUT = "us";
+  };
+
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
-
-  console.useXkbConfig = true;
 }
