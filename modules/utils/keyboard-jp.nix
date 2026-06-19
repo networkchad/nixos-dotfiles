5618520a67ccd4917b09d47475be18f23636fe8d
anon <ilovearchbtw@proton.me>
2026-06-19 17:45:32 +0800

    fix

--- modules/utils/keyboard-jp.nix
+++ modules/utils/keyboard-jp.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
 {
+  environment.sessionVariables = {
+    XKB_DEFAULT_LAYOUT = "jp";
+  };
+
   services.xserver.xkb = {
     layout = "jp";
     variant = "";
--- modules/utils/keyboard-us.nix
+++ modules/utils/keyboard-us.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
 {
+  environment.sessionVariables = {
+    XKB_DEFAULT_LAYOUT = "us";
+  };
+
   services.xserver.xkb = {
     layout = "us";
     variant = "";
