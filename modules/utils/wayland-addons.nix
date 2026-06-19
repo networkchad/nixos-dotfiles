84e8cbeed5e762c31b898026b6f4a55f76d0c4e2
anon <ilovearchbtw@proton.me>
2026-06-19 19:26:33 +0800

    fcitx5 wayland

--- modules/utils/wayland-addons.nix
+++ modules/utils/wayland-addons.nix
@@ -3,6 +3,10 @@
 {
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1"; 
+    IMMODULE = "fcitx";
+    GTK_IM_MODULE = "fcitx";
+    QT_IM_MODULE = "fcitx";
+    XMODIFIERS = "@im=fcitx";
   };
 
   i18n.inputMethod.fcitx5.waylandFrontend = true;
