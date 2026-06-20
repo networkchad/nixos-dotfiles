6a96a79b9326b127b2e722316cbd600bd9aa3214
anon <ilovearchbtw@proton.me>
2026-06-20 16:41:59 +0800

    bug

--- modules/pkgs/foot.nix
+++ modules/pkgs/foot.nix
@@ -1,16 +1,18 @@
 { config, pkgs, ... }:
 
 {
-  programs.foot = {
-    enable = true;
-    settings = {
-      main = {
-        font = "monospace:size=11";
-        dpi-aware = "yes";
-      };
-      colors-dark = {
-        alpha = "0.4";
-      };
-    };
-  };
+  programs.foot = {
+    enable = true;
+    settings = {
+      main = {
+        font = "monospace:size=11";
+        dpi-aware = "yes";
+      };
+      colors-dark = {
+        alpha = "0.4";
+        background = "111111";
+        foreground = "cccccc";
+      };
+    };
+  };
 }
