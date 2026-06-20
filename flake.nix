55561656f6db27bf35a9d8ff5d7ffbd82666bb22
anon <ilovearchbtw@proton.me>
2026-06-20 11:16:46 +0800

    fix flake

--- flake.nix
+++ flake.nix
@@ -3,12 +3,11 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-
     home-manager.url = "github:nix-community/home-manager/release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
       lib = nixpkgs.lib;
@@ -30,28 +29,25 @@
           inherit system;
 
           specialArgs = {
-            inherit sessionType inputs;
+            inherit sessionType;
           };
 
-          modules =
-            [
-              ./hosts/${hostName}/configuration.nix
+          modules = [
+            ./hosts/${hostName}/configuration.nix
 
-              home-manager.nixosModules.home-manager
-              {
-                home-manager.useGlobalPkgs = true;
-                home-manager.useUserPackages = true;
-                home-manager.backupFileExtension = "backup";
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "backup";
 
-                home-manager.users = lib.genAttrs users (user:
-                  import ./hosts/${hostName}/home/${user}-${sessionType}.nix
-                );
-              }
-            ]
-            ++ lib.optionals (sessionType == "wayland") [
-              inputs.wayland-pipewire-idle-inhibit.nixosModules.default
-            ];
+              home-manager.users = lib.genAttrs users (user:
+                import ./hosts/${hostName}/home/${user}-${sessionType}.nix
+              );
+            }
+          ];
         };
+
     in {
       nixosConfigurations = builtins.mapAttrs mkSystem hosts;
     };
