06c19262e734f7b1addd850fbaaa3dc4abd1d323
anon <ilovearchbtw@proton.me>
2026-06-20 16:39:44 +0800

    clean

--- modules/pkgs/foot.nix
+++ modules/pkgs/foot.nix
@@ -1,26 +1,16 @@
 { config, pkgs, ... }:
 
 {
-  programs.foot = {
-    enable = true;
-    settings = {
-      main = {
-        font = "monospace:size=11";
-        dpi-aware = "yes";
-      };
-      colors-dark = {
-        alpha = "0.4";
-        background = "111111";
-        foreground = "cccccc";
-        regular0 = "000000";
-        regular1 = "cd0000";
-        regular2 = "5c5cff";
-        regular3 = "cdcd00";
-        regular4 = "0000ee";
-        regular5 = "cd00cd";
-        regular6 = "00cdcd";
-        regular7 = "e5e5e5";
-      };
-    };
-  };
+  programs.foot = {
+    enable = true;
+    settings = {
+      main = {
+        font = "monospace:size=11";
+        dpi-aware = "yes";
+      };
+      colors-dark = {
+        alpha = "0.4";
+      };
+    };
+  };
 }
--- modules/pkgs/swaylock.nix
+++ modules/pkgs/swaylock.nix
@@ -4,18 +4,13 @@
   programs.swaylock = {
     enable = true;
     settings = {
+      color = "808080";
       color = "000000";
       font-size = 24;
       indicator-idle-visible = false;
       indicator-radius = 100;
       line-color = "ffffff";
       show-failed-attempts = true;
-      key-hl-color = "eeeeee";
-      inside-input-color = "000000";
-      ring-input-color = "eeeeee";
-      inside-wrong-color = "f4cccc";
-      ring-wrong-color = "f4cccc";
-      text-wrong-color = "000000";
     };
   };
 }
