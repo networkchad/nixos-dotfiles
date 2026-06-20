e83aa5e68d6daf13b1a344014f3d6e02ff91f7b3
anon <ilovearchbtw@proton.me>
2026-06-20 09:35:34 +0800

    swaylock style

--- modules/pkgs/swaylock.nix
+++ modules/pkgs/swaylock.nix
@@ -3,34 +3,13 @@
 {
   programs.swaylock = {
     enable = true;
-
     settings = {
-      color = "000000";
-
-      inside-color = "000000cc";
-      ring-color = "eeeeee";
-      text-color = "eeeeee";
-      line-color = "00000000";
-
-      inside-ver-color = "000000cc";
-      ring-ver-color = "eeeeee";
-      text-ver-color = "eeeeee";
-
-      inside-wrong-color = "000000cc";
-      ring-wrong-color = "f4cccc";
-      text-wrong-color = "f4cccc";
-
-      key-hl-color = "eeeeee";
-      bs-hl-color = "f4cccc";
-
-      separator-color = "00000000";
-
-      indicator = true;
-      indicator-radius = 120;
-      indicator-thickness = 8;
-
-      clock = true;
-      screenshots = false;
+      color = "808080";
+      font-size = 24;
+      indicator-idle-visible = false;
+      indicator-radius = 100;
+      line-color = "ffffff";
+      show-failed-attempts = true;
     };
   };
 }
