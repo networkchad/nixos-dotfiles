197d4ed386ae7e15486f6da942c54d35b751bd27
anon <ilovearchbtw@proton.me>
2026-06-21 08:03:12 +0800

    sytle

--- modules/pkgs/swaylock.nix
+++ modules/pkgs/swaylock.nix
@@ -9,6 +9,11 @@
       indicator-idle-visible = false;
       indicator-radius = 100;
       show-failed-attempts = true;
+      key-hl-color = "ffffff";
+      inside-wrong-color = "f4cccc";
+      ring-wrong-color = "f4cccc";
+      text-wrong-color = "000000";
+      line-wrong-color = "f4cccc";
     };
   };
 }
