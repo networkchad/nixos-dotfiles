8e92a3c32a068e058796984e932a848a30adf743
anon <ilovearchbtw@proton.me>
2026-06-21 08:04:56 +0800

    simple

--- modules/pkgs/swaylock.nix
+++ modules/pkgs/swaylock.nix
@@ -9,11 +9,6 @@
       indicator-idle-visible = false;
       indicator-radius = 100;
       show-failed-attempts = true;
-      key-hl-color = "ffffff";
-      inside-wrong-color = "f4cccc";
-      ring-wrong-color = "f4cccc";
-      text-wrong-color = "000000";
-      line-wrong-color = "f4cccc";
     };
   };
 }
