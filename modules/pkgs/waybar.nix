fd3c517e198ea6abf1319328344e3b8f44525096
anon <ilovearchbtw@proton.me>
2026-06-23 21:49:36 +0800

    waybar

--- modules/pkgs/waybar.nix
+++ modules/pkgs/waybar.nix
@@ -1,13 +1,65 @@
 {
   programs.waybar = {
     enable = true;
+
     settings.mainBar = {
-      modules-center = [ "clock" ];
+      modules-left = [ ];
+
+      modules-center = [
+        "custom/internet"
+        "custom/vpn"
+        "custom/volume"
+        "cpu"
+        "memory"
+        "battery"
+        "clock"
+      ];
+
+      modules-right = [ ];
+
+      "custom/internet" = {
+        exec = "printf 'Net:%s' \"$(${HOME}/.config/slstatus/scripts/internet.sh)\"";
+        interval = 5;
+      };
+
+      "custom/vpn" = {
+        exec = "printf 'VPN:%s' \"$(${HOME}/.config/slstatus/scripts/vpn.sh)\"";
+        interval = 5;
+      };
+
+      "custom/volume" = {
+        exec = "printf 'Vol:%s' \"$(${HOME}/.config/slstatus/scripts/volume.sh)\"";
+        interval = 1;
+      };
+
+      cpu = {
+        format = "CPU:{usage}%";
+      };
+
+      memory = {
+        format = "RAM:{}%";
+      };
+
+      battery = {
+        format = "Bat:{capacity}%";
+      };
+
+      clock = {
+        format = "{:%Y-%m-%d %H:%M}";
+      };
     };
+
     style = ''
       window#waybar {
-        background: #000000;
-        color: #ffffff;
+        background: #000;
+        color: #fff;
+      }
+
+      * {
+        border: none;
+        border-radius: 0;
+        min-height: 0;
+        padding: 0 3px;
       }
     '';
   };
