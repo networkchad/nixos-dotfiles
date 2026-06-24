fac36d1187f27ade3a2b7d755a27d9e96565f2c7
anon <ilovearchbtw@proton.me>
2026-06-24 19:52:21 +0800

    greetd

--- modules/utils/greetd.nix
+++ modules/utils/greetd.nix
@@ -4,6 +4,7 @@ let
   sessions = {
     "dwl"  = "slstatus -s | dwl";
     "niri" = "${config.programs.niri.package}/bin/niri-session";
+    "dwm"  = "${pkgs.xorg.xinit}/bin/startx ${config.services.xserver.windowManager.dwm.package}/bin/dwm";
   };
 
   sessionCommand = sessions.${sessionType} or (throw "Unsupported sessionType: ${sessionType}");
