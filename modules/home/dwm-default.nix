b0d5515fc8ba1f7b3c3f0cad340c74bbedf654fb
anon <ilovearchbtw@proton.me>
2026-06-25 21:06:37 +0800

    switch

--- modules/home/dwm-default.nix
+++ modules/home/dwm-default.nix
@@ -31,10 +31,10 @@
     initExtra = ''
       fcitx5 -d &
       slstatus &
+      xrandr --output eDP-1 --auto --primary --output HDMI-1-0 --mode 2560x1440 --rate 144 --right-of eDP-1 &
       feh --bg-fill $HOME/.config/wallpapers/bg.png &
       xset s 300
       xss-lock -- slock &
-      xrandr --output eDP-1 --auto --primary --output HDMI-1-0 --mode 2560x1440 --rate 144 --right-of eDP-1 &
     '';
   };
 }
