f581cea7136b96221daf2d3d2459437fa67aeccb
anon <ilovearchbtw@proton.me>
2026-06-26 21:17:54 +0800

    yazi

---
+++ modules/pkgs/yazi.nix
@@ -0,0 +1,43 @@
+{ pkgs, sessionType, ... }:
+
+{
+  programs.yazi = {
+    enable = true;
+    enableBashIntegration = true; 
+    enableZshIntegration = true;  
+
+    settings = {
+      opener = {
+        image_viewer = {
+          exec = let
+            viewers = {
+              "dwm" = "${pkgs.feh}/bin/feh --start-at \"$1\" .";
+              "dwl" = "${pkgs.imv}/bin/imv -n \"$1\" .";
+            };
+          in viewers.${sessionType} or "${pkgs.imv}/bin/imv -n \"$1\" .";
+          desc = "View Image Directory";
+        };
+
+        media_player = {
+          exec = "${pkgs.mpv}/bin/mpv --force-window=yes \"$@\"";
+          desc = "Play Media (Forced Window)";
+        };
+
+        generic_browser = {
+          exec = "xdg-open \"$@\"";
+          desc = "Open with Default Browser/Viewer";
+        };
+      };
+
+      open = {
+        rules = [
+          { mime = "image/*"; use = "image_viewer"; orphan = true; }
+          { mime = "video/*"; use = "media_player"; orphan = true; }
+          { mime = "audio/*"; use = "media_player"; orphan = true; }
+          { mime = "text/html"; use = "generic_browser"; orphan = true; }
+          { mime = "application/pdf"; use = "generic_browser"; orphan = true; }
+        ];
+      };
+    };
+  };
+}
